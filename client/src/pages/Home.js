a99c931269d8accc8d4221138f4d86b21d90d982
Maximus Cristian <mcristia2938@gmail.com>
2022-08-29 12:30:00 -0700

    changed the colors on the home.js so that it all flows

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,45 +6,97 @@ import UserProfile from "../components/UserProfile";
 import UserFeed from "../components/UserFeed";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import logo from "../assets/img/logo.png"
+import logo from "../assets/img/logo.png";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from 'react-bootstrap/Image'
+import Image from "react-bootstrap/Image";
 
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
+import { color } from "@mui/system";
 
 const Home = () => {
   return (
-    <Container fluid='true'>
+    <Container fluid="true">
       <Row className="row justify-content-between">
-
         {Auth.loggedIn() ? (
           <>
             <Col
               sm={2}
-              style={{ background: "grey", minHeight: "90vh", flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }} className="d-flex flex-grow-1"
-              >
+              style={{
+                background: "white",
+                minHeight: "90vh",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+              className="d-flex flex-grow-1"
+            >
               <div>
                 <div>
-                  <Image style={{ width: '12vh', marginLeft: '25%', marginBottom: '1vh', marginTop: '1vh', }} fluid="true" src={logo} />
+                  <Image
+                    style={{
+                      width: "13vh",
+                      height: "300px",
+                      marginLeft: "10%",
+                      marginBottom: "1vh",
+                      marginTop: "1vh",
+                    }}
+                    fluid="true"
+                    src={logo}
+                  />
                 </div>
-                <div style={{ marginTop: '10%', textAlign: 'center',  }}>
+                <div style={{ marginTop: "10%", textAlign: "center" }}>
                   <div>
-                    <Button style={{ width: '20vh', borderStyle: 'solid', marginTop: '2vh' }} variant='warning'>Events</Button>
+                    <Button
+                      style={{
+                        width: "20vh",
+                        borderStyle: "solid",
+                        marginTop: "2vh",
+                        backgroundColor: "purple",
+                      }}
+                    >
+                      Events
+                    </Button>
                   </div>
                   <div>
-                    <Button style={{ width: '20vh', borderStyle: 'solid', marginTop: '2vh' }} variant='warning'>Friends</Button>
+                    <Button
+                      style={{
+                        width: "20vh",
+                        borderStyle: "solid",
+                        marginTop: "2vh",
+                        backgroundColor: "purple",
+                      }}
+                    >
+                      Friends
+                    </Button>
                   </div>
                   <div>
-                    <Button style={{ width: '20vh', borderStyle: 'solid', marginTop: '2vh' }} variant='warning'>Groups</Button>
+                    <Button
+                      style={{
+                        width: "20vh",
+                        borderStyle: "solid",
+                        marginTop: "2vh",
+                        backgroundColor: "purple",
+                      }}
+                    >
+                      Groups
+                    </Button>
                   </div>
                 </div>
               </div>
 
-              <div className=" mt-auto" style={{marginBottom: '10%'}}>
-                <button type="button" className="btn btn-warning mt-4" style={{ width: '20vh', borderStyle: 'solid', marginTop: '2vh', }}>
+              <div className=" mt-auto" style={{ marginBottom: "10%" }}>
+                <button
+                  type="button"
+                  className="btn  mt-4"
+                  style={{
+                    width: "20vh",
+                    borderStyle: "solid",
+                    backgroundColor: "purple",
+                  }}
+                >
                   {/* this is not using the Link component to logout or user and then refresh the application to the start */}
                   <a href="/" onClick={() => Auth.logout()}>
                     Logout
@@ -53,41 +105,60 @@ const Home = () => {
               </div>
             </Col>
 
-
             <Col
               sm={8}
-              style={{ background: "orange" }}
+              style={{ background: "white" }}
               className="d-flex flex-grow-1"
             >
               <UserFeed />
             </Col>
 
-            <Col sm={2} style={{ background: "grey", }} className="d-flex flex-grow-1">
+            <Col
+              sm={2}
+              style={{ background: "white" }}
+              className="d-flex flex-grow-1"
+            >
               <UserProfile />
-              
             </Col>
           </>
         ) : (
           <>
             <Col
               sm={2}
-              style={{ background: "grey", minHeight: "90vh", marginLeft: '50px' }}
+              style={{
+                background: "white",
+                minHeight: "90vh",
+                marginLeft: "50px",
+              }}
             >
               <div>
-                <Image style={{ width: '12vh', marginLeft: '22%', marginBottom: '1vh', marginTop: '1vh', }} fluid="true" src={logo} />
+                <Image
+                  style={{
+                    width: "12vh",
+                    marginLeft: "22%",
+                    marginBottom: "1vh",
+                    marginTop: "1vh",
+                  }}
+                  fluid="true"
+                  src={logo}
+                />
               </div>
 
-              <div style={{ marginTop: '25%' }} className="text-center">
-                <h1> WYD...</h1>
+              <div style={{ marginTop: "25%" }} className="text-center">
+                <h1> Welcome to WYD!</h1>
                 <br />
-                <h6> Connect with Friends... Get Logged In or Signup</h6>
+                <h6>
+                  Whatever you’re looking to do this year, WYD can help. For
+                  years, people have turned to WYD to meet people, make friends,
+                  find support, and explore their interests. Thousands of events
+                  are happening every day—join in on the fun!
+                </h6>
               </div>
             </Col>
 
-
             <Col
               sm={9}
-              style={{ background: "orange" }}
+              style={{ background: "white" }}
               className="text-center d-flex justify-content-center"
             >
               <div style={{ marginTop: "10%" }}>
@@ -102,7 +173,7 @@ const Home = () => {
             </Col>
           </>
         )}
-        <footer className="footer" style={{backgroundColor: '#F5F3F4'}}>
+        <footer className="footer" style={{ backgroundColor: "#F5F3F4" }}>
           <div className="container">
             <div className="row">
               <div className="footer-col col">
@@ -165,8 +236,8 @@ const Home = () => {
                   <a href="https://twitter.com/GroupWyd">
                     <i className="fab fa-twitter"></i>
                   </a>
-                  <a href="#">
-                    <i className="fab fa-facebook-f"></i>
+                  <a href="https://github.com/RPAPE3/WYD">
+                    <i className="fab fa-github"></i>
                   </a>
                   <a href="https://www.instagram.com/groupwyd/">
                     <i className="fab fa-instagram"></i>
